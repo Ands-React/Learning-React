92a05f543bd0556869f290cb0e9f784f56c8e0ca
AndsLiao <budliao1016@gmail.com>
2025-07-21 16:46:33 +0800

    分離後端合併

--- personalWeb/backend/profile.js
+++ personalWeb/backend/profile.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import session from "express-session";
 import cors from "cors";
 import multer from "multer";
 import cookieParser from "cookie-parser";
@@ -11,9 +10,7 @@ import {
   editArticleToDB,
   deleteArticlesFromDB,
 } from "./mongoDB.js";
-import { RedisStore } from "connect-redis";
-import { createClient } from "redis";
-
+import jwt from "jsonwebtoken";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads");
@@ -26,25 +23,35 @@ const upload = multer({ storage: storage });
 
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const EXPIRATION_TIMES = {
   ONE_MINUTE: 1000 * 60,
   HALF_HOUR: 30 * 60 * 1000,
   ONE_HOUR: 60 * 60 * 1000,
   ONE_DAY: 24 * 60 * 60 * 1000,
 };
-// let redisClient = createClient({
-//   socket: {
-//     host: "localhost", // 你的 Redis 伺服器位址
-//     port: 6379, // 預設端口
-//   },
-// });
 
-// await redisClient.connect().catch(console.error);
+const generateAccessToken = (username) => {
+  return jwt.sign(
+    { username: username, signInTime: Date.now() },
+    process.env.ACCESS_SECRET,
+    { expiresIn: "30m" }
+  );
+};
 
-// let redisStore = new RedisStore({
-//   client: redisClient,
-// });
+function verifyToken(req, res, next) {
+  // const token = req.cookie.token  直接從 Cookie 讀取 Token
+  const token = req.headers.authorization.replace("Bearer ", "")
+  if (!token) return res.status(401).end();
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+    req.user = decoded; // 存入 req.user
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "登入已逾時請重新登入", isValid: false });
+  }
+}
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -64,57 +71,34 @@ app.use(
   })
 );
 
-app.use(
-  session({
-    // store: redisStore,
-    secret: "hashcat",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false, // 設置 secure 為 true 需要 HTTPS
-      maxAge: EXPIRATION_TIMES.HALF_HOUR, // 設置過期時間 30分鐘
-    },
-  })
-);
-
-const checkSession = (req, res, next) => {
-  if (!req.session || !req.session.username) {
-    return res.status(401).send({ message: "未授權操作請重新登入" });
-  }
-  next(); // 通過驗證，繼續執行後續請求
-};
-
 app.post("/login", (req, res) => {
+  const { username, password } = req.body;
   if (
-    req.body.username === process.env.USER_ACCOUNT &&
-    req.body.password === process.env.USER_PASSWORD
+    username === process.env.USER_ACCOUNT &&
+    password === process.env.USER_PASSWORD
   ) {
-    req.session.username = req.body.username;
-    console.log(req.session);
-    res.status(200).send({ message: "登入成功", isValid: true });
+    const token = generateAccessToken(username);
+    /* res.cookie("token", token, {
+      httpOnly: true, // 防止 JavaScript 存取
+      secure: false, // 只允許 HTTPS
+      sameSite: "lax", // 防止 CSRF 攻擊
+      maxAge: EXPIRATION_TIMES.HALF_HOUR, // 半小時
+    }); */
+    res.status(200).send({ message: "登入成功", isValid: true, token: token });
   } else {
     res.status(400).send({ message: "帳號或密碼有誤" });
   }
 });
 
-app.post("/logout", (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      res.status(500).send({ message: "伺服器內部發生錯誤" });
-    } else {
-      res.clearCookie("connect.sid", {
-        path: "/",
-        domain: "localhost",
-      });
-      res.status(200).send({ message: "已成功登出", isValid: false });
-    }
-  });
+app.post("/logout",verifyToken ,(req, res) => {
+  res.status(200).send({ message: "已成功登出", isValid: false });
 });
 
 // 獲取mongoDB文章資料
 app.post("/getArticles", async (req, res) => {
   try {
     const { articles, articleOrder } = await getArticlesByDB();
+    
     res.status(200).send({ articles, articleOrder });
   } catch (err) {
     res.status(404).send({ message: err.message });
@@ -122,42 +106,48 @@ app.post("/getArticles", async (req, res) => {
 });
 
 // 新增文章至 mongoDB
-app.post("/insertArticle", checkSession, async (req, res) => {
+app.post("/insertArticle", verifyToken, async (req, res) => {
   try {
     const article = req.body;
-    console.log("要新增的文章: ", article);
+    // console.log("要新增的文章: ", article);
     const result = await insertArticleToDB(article.content);
+    console.log("新增文章成功")
     res.status(200).send({ message: result, article: article });
   } catch (err) {
+    console.error("新增文章失敗", err)
     res.status(500).send({ message: err.message });
   }
 });
 
 // 編輯文章至 mongoDB
-app.post("/editArticle", checkSession, async (req, res) => {
+app.post("/editArticle", verifyToken, async (req, res) => {
   try {
     const article = req.body;
-    console.log("要新增的文章: ", article);
+    // console.log("要編輯的文章: ", article);
     const result = await editArticleToDB(article.id, article.content);
+        console.log("編輯文章成功")
     res.status(200).send({ message: result, article: article });
   } catch (err) {
+    console.error("編輯文章失敗", err)
     res.status(500).send({ message: err.message });
   }
 });
 
 // 刪除文章從 mongoDB
-app.post("/deleteArticle", checkSession, async (req, res) => {
+app.post("/deleteArticle", verifyToken, async (req, res) => {
   try {
     const selectedID = req.body.id;
     const result = await deleteArticlesFromDB(selectedID);
+    console.log("刪除文章成功")
     res.status(200).send({ message: result, selectedID: selectedID });
   } catch (err) {
+    console.error("刪除文章失敗", err)
     res.status(500).send({ message: err.message });
   }
 });
 
 // 處理上傳頭像
-app.post("/uploadImg", checkSession, upload.single("image"), (req, res) => {
+app.post("/uploadImg", verifyToken, upload.single("image"), (req, res) => {
   res.status(200).send({ message: "頭像上傳成功" });
 });
 app.get("/getImg", (req, res) => {
@@ -170,35 +160,23 @@ app.get("/getImg", (req, res) => {
 });
 
 // 所有新增、編輯、刪除的模式，都會先透過該路由檢查session並延長。
-app.get("/check-session", (req, res) => {
-  if (!req.session.username) {
-    if (req.query.state === "islogged") {
-      console.log("F5 進入檢查");
-      console.log(req.session);
-      res.status(401).end();
-    } else {
-      res.status(401).send({ message: "登入已逾時請重新登入", isValid: false });
-    }
+app.get("/check-session", verifyToken, (req, res) => {
+  if (req.query.state === "islogged") {
+    res.status(200).send({ message: "歡迎回來", isValid: true });
   }
-  if (req.session.username) {
-    if (req.query.state === "islogged") {
-      res.status(200).send({ message: "歡迎回來", isValid: true });
-    }
-    if (req.query.state === "insert" || req.query.state === "edit") {
-      // req.sessionStore.touch(req.sessionID, req.session, (err) => {
-      //   if (err) console.error("更新 Redis TTL 失敗:", err);
-      // });
-      console.log("輪詢開始");
+  if (req.query.state === "insert" || req.query.state === "edit") {
+    console.log("輪詢開始");
 
-      const cookieSideID = req.cookies["connect.sid"];
+    const token = generateAccessToken(req.user.username);
 
-      req.session.touch();
-      res.cookie("connect.sid", cookieSideID, {
-        maxAge: EXPIRATION_TIMES.HALF_HOUR,
-        secure: false,
-      });
-      res.status(200).send({ state: req.query.state });
-    }
+    /* res.cookie("token", token, {
+      httpOnly: true, // 防止 JavaScript 存取
+      secure: false, // 只允許 HTTPS
+      sameSite: "lax", // 防止 CSRF 攻擊
+      maxAge: EXPIRATION_TIMES.HALF_HOUR, // 7 天
+    }); */
+
+    res.status(200).send({ state: req.query.state, token: token });
   }
 });
 
